54e7c7605165a732b232ab373a7c8f6de617e707
Qian Ma <qian.ma3@dcmail.ca>
2020-01-16 19:55:41 -0500

    site created

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// window.onload = function(){
+//     let bakerimg = document.querySelector('.bakerimg');
+
+//     bakerimg.addEventListener("mouseenter", TweenMax.to('.bakernote',0.5, {
+//         height:1
+//     }));
+
+//     bakerimg.addEventListener("mouseleave", TweenMax.to('.bakernote',0.5, {
+//         height:0
+//     }));
+    
+// }
